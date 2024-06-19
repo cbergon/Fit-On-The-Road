0a3f950b2155848c18d9223eee5ab3599b97607d
Clémence Bergon <clem.bergon@gmail.com>
2024-06-19 23:26:51 +0200

    fix: vid link

--- src/modules/landing/Hero.tsx
+++ src/modules/landing/Hero.tsx
@@ -5,6 +5,7 @@ import Container from "@mui/material/Container";
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
 import { alpha } from "@mui/material";
+import Video from "./Video";
 
 function Hero() {
   // #region SCROLLING FUNCTION
@@ -94,32 +95,7 @@ function Hero() {
             </Button>
           </Stack>
         </Stack>
-        <Box
-          id="video"
-          sx={() => ({
-            mt: { xs: 8, sm: 10 },
-            alignSelf: "center",
-            height: { xs: 200, sm: 700 },
-            width: "100%",
-            backgroundSize: "no-repeat",
-            backgroundPosition: "center center",
-            borderRadius: "10px",
-            outline: "1px solid",
-            outlineColor: alpha("#ED130D", 0.1),
-            boxShadow: `0 0 24px 12px ${alpha("#ED130D", 0.2)}`,
-          })}
-        >
-          <iframe
-            width="100%"
-            height="100%"
-            className="rounded-[10px]"
-            src="https://www.youtube.com/embed/_Zw_grCr49o?si=6jdvO5kJp0yBv0gQ"
-            title="YouTube video player"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            referrerPolicy="strict-origin-when-cross-origin"
-            allowFullScreen
-          />
-        </Box>
+        <Video />
       </Container>
     </Box>
   );
